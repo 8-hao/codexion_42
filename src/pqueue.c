02a61d7d8f855854688d925a0bfe44f8650eeeda
8-hao <hananebmd077@gmail.com>
2026-06-21 09:11:33 +0100

    pqueue

--- src/pqueue.c
+++ src/pqueue.c
@@ -16,28 +16,81 @@ Queue *newNode(int val, int index)
 
 void add_back(Queue **head, Queue *node)
 {
-    Queue **second = head;
-    if (head == NULL){
+    Queue *second = *head;
+    if (*head == NULL){
         *head = node;
         return;
     }
 
-    while((*second)->next){
-        *second =(*second)->next;
+    while(second->next){
+        second =second->next;
     }
-    (*second)->next = node;
+    second->next = node;
 }
 
+int queuelen(Queue *head)
+{
+    int i;
+
+    i = 0;
+    while(head)
+    {
+        i++;
+        head = head->next;
+    }
+    return i;
+}
+
+void sort_min(Queue **head)
+{
+    int i;
+    int ql;
+    Queue *helper;
+    Queue *n;
+    int tval;
+    int tin;
+
+    i = 0;
+    ql = queuelen(*head);
+    while(i < ql)
+    {
+        helper = *head;
+        while(helper && helper->next)
+        {
+            if (helper->index > (helper->next)->index)
+            {
+                tval = helper-> val;
+                tin = helper-> index;
+                helper->index = (helper->next)->index;
+                (helper->next)->index = tin;
+                helper->val = (helper->next)->val;
+                (helper->next)->val = tval;
+
+            }
+            helper= helper->next;
+        }
+        i++;
+    }
+}
 
 int main()
 {
     Queue *head = NULL; 
-    Queue *s1 = newNode(1, 10);
-    Queue *s2 = newNode(10, 11);
+    Queue *s1 = newNode(1, 1);
+    Queue *s2 = newNode(8, 8);
+    Queue *s3 = newNode(2, 2);
+    Queue *s4 = newNode(4, 4);
+    Queue *s5 = newNode(7, 7);
+    Queue *s6 = newNode(0, 0);
     add_back(&head, s1);
     add_back(&head, s2);
-    printf("hillow\n");
-    while(head->next){
+    add_back(&head, s3);
+    add_back(&head, s4);
+    add_back(&head, s5);
+    add_back(&head, s6);
+    printf("^^^^^^^^^^^^\n");
+    sort_min(&head);
+    while(head){
         printf("%d\n",head->val);
         head = head->next;
     }
